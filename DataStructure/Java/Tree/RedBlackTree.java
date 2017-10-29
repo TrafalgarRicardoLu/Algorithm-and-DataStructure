487ea751669638ad0681770b94d1fa51d0e751de
TrafalgarRicardoLu <trafalgarricardolu@gmail.com>
2017-10-29 13:31:36 +0800

    Add RedBlackTree written by Java

---
+++ DataStructure/Java/Tree/RedBlackTree.java
@@ -0,0 +1,195 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class RedBlackTree<Key extends Comparable<Key>, Value> {
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    private class Node {
+        private Key key;
+        private Value value;
+        private Node left, right;
+        private boolean color;
+
+        public Node(Key key, Value value) {
+            this.key = key;
+            this.value = value;
+            this.color = RED;
+        }
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) return false;
+        return x.color == RED;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+
+        h.right = x.left;
+        x.left = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        return x;
+    }
+
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+
+        h.left = x.right;
+        x.right = h;
+
+        x.color = h.color;
+        h.color = RED;
+
+        return x;
+    }
+
+    private void flipColors(Node h) {
+        h.color = !h.color;
+        h.left.color = !h.left.color;
+        h.right.color = !h.right.color;
+    }
+
+
+    private Node balance(Node h) {
+        if (isRed(h.right) && !isRed(h.left)) h = rotateLeft(h);
+        if (isRed(h.left) && isRed(h.left.left)) h = rotateRight(h);
+        if (isRed(h.left) && isRed(h.right)) flipColors(h);
+        return h;
+    }
+
+    public Value get(Key key) {
+        return get(root, key);
+    }
+
+    private Value get(Node x, Key key) {
+        if (x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) return get(x.left, key);
+        else if (cmp > 0) return get(x.right, key);
+        else return x.value;
+    }
+
+    public void put(Key key, Value value) {
+        root = put(root, key, value);
+        root.color = BLACK;
+    }
+
+    private Node put(Node node, Key key, Value value) {
+        if (node == null)
+            return new Node(key, value);
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, value);
+        else if (cmp > 0)
+            node.right = put(node.right, key, value);
+        else
+            node.value = value;
+
+        return balance(node);
+    }
+
+
+    public void deleteMin() {
+        root = deleteMin(root);
+        root.color = BLACK;
+    }
+
+    private Node deleteMin(Node h) {
+        if (h.left == null) return null;
+        if (!isRed(h.left) && !isRed(h.left.left))
+            h = moveRedLeft(h);
+        h.left = deleteMin(h.left);
+        return balance(h);
+    }
+
+    private Node moveRedLeft(Node h) {
+        flipColors(h);
+        if (isRed(h.right.left)) {
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    private Node moveRedRight(Node h) {
+        flipColors(h);
+        if (isRed(h.left.left)) {
+            h = rotateLeft(h);
+            flipColors(h);
+        }
+        return h;
+    }
+
+    private Node min(Node node) {
+        while (node.left != null)
+            node = node.left;
+        return node;
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+        root.color = BLACK;
+    }
+
+    private Node delete(Node h, Key key) {
+        if (key.compareTo(h.key) < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left))
+                h = moveRedLeft(h);
+            h.left = delete(h.left, key);
+        } else {
+            if (isRed(h.left))
+                h = rotateRight(h);
+
+            if (key.compareTo(h.key) == 0 && h.right == null)
+                return null;
+            if (!isRed(h.right) && !isRed(h.right.left))
+                h = moveRedRight(h);
+            if (key.compareTo(h.key) == 0) {
+                h.value = get(h.right, min(h.right).key);
+                h.key = min(h.right).key;
+                h.right = deleteMin(h.right);
+            } else h.right = delete(h.right, key);
+        }
+        return balance(h);
+    }
+
+    public void layerTraversal() {
+        layerTraversal(root);
+    }
+
+
+    private void layerTraversal(Node node) {
+        Queue<Node> s = new LinkedList<>();
+        s.add(node);
+        Node curNode;
+        Node nlast = null;
+        Node last = node;
+        while (!s.isEmpty()) {
+            curNode = s.poll();
+            System.out.print(curNode.key + " ");
+            if (curNode.left != null) {
+                nlast = curNode.left;
+                s.add(curNode.left);
+            }
+            if (curNode.right != null) {
+                nlast = curNode.right;
+                s.add(curNode.right);
+            }
+            if (curNode == last) {
+                System.out.println();
+                last = nlast;
+            }
+        }
+    }
+
+
+}
